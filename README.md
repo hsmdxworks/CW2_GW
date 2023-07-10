34b68173d91f2801ec740d6f3e6e67616576c2e4
Harpal Singh <hh797@live.mdx.ac.uk>
2023-07-10 20:22:08 +0100

    readme updated

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Coursework 2 - Group Work
 
-### URL - Frontend
-https://hsmdxworks.github.io/CW2_GW/
+### URL - Frontend (VueJS APP)
+(It should be work when server is running from https://hsmdxworks.github.io/CW2_GW/vueapp.html)
+https://hsmdxworks.github.io/CW2_GW/public/vueapp.html
+
+### URL - ExpressJS (It should be work when server is running from https://hsmdxworks.github.io/CW2_GW/vueapp.html)
+https://hsmdxworks.github.io/CW2_GW/lessons
+https://hsmdxworks.github.io/CW2_GW/users
 
 ### REQUIREMENTS
 - `` Done
--- server.js
+++ server.js
@@ -4,44 +4,40 @@ const app = express();
 // Require public folder to be seen for template
 app.use(express.static("public"));
 
+// first get request
 app.get("/lessons", function (request, response) {
     const data = [
         {
             "topic": "Math",
             "location": "hendon",
             "price": "100"
-        },
-        {
+        }, {
             "topic": "Math",
             "location": "hendon",
             "price": "100"
-        },
-        {
+        }, {
             "topic": "Math",
             "location": "hendon",
             "price": "100"
-        },
-        {
+        }, {
             "topic": "Math",
             "location": "hendon",
             "price": "100"
-        },
-        {
+        }, {
             "topic": "Math",
             "location": "hendon",
             "price": "100"
         },
     ];
-
     response.send(data);
 });
 
-app.get("/users", function(request,response){
-    const users = [
-        {
-            "username":"Harpal Singh",
-            "password":"anythingispossible"
-        }
+// Second get request
+app.get("/users", function (request, response) {
+    const users = [{
+        "username": "Harpal Singh",
+        "password": "anythingispossible"
+    }
     ];
     response.send(users);
 });
